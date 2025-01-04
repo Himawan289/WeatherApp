56e574fb8f471aef6e770fd21174d8b0ca346d8e
Himawan289 <160473999+Himawan289@users.noreply.github.com>
2025-01-04 16:39:21 +0700

    UI

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue, .white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("Asem Mulya, Indonesia")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundStyle(.white)
+                    .padding()
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
