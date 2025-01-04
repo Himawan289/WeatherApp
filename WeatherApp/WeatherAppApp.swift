dd94782b84f7d19627275f2d6135e9079f7e6beb
Himawan289 <160473999+Himawan289@users.noreply.github.com>
2025-01-04 15:31:43 +0700

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Dimas on 04/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
